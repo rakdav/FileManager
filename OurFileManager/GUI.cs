d2fe46978e4505507ce44bc95f1ba74e7072d217
Andrews <rakdav@inbox.ru>
2024-09-04 21:21:24 +0200

    LKast

---
+++ OurFileManager/GUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFileManager
+{
+    internal static class GUI
+    {
+        public static void DrawHorizontalLine()
+        {
+            string buf = "";
+            buf += "│";
+            for (int i = 0; i < Config.WindowWidth; i++)
+            {
+                buf += "─";
+            }
+            buf += "│";
+            Console.WriteLine(buf);
+        }
+    }
+}
